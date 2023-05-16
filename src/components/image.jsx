f65ea84a8cda23207faebed0ac4bd5fe795067bb
ivan <ivanteens@gmail.com>
2023-05-16 18:39:19 +0700

    Add Batik Kayu Link

--- src/components/image.jsx
+++ src/components/image.jsx
@@ -1,14 +1,21 @@
 import React from "react";
 
-export const Image = ({ title, largeImage, smallImage }) => {
+export const Image = ({ title, smallImage,link }) => {
   return (
     <div className="portfolio-item">
       <div className="hover-bg">
         {" "}
-        <div className="hover-text">
-          <h4>{title}</h4>
-        </div>
-        <img src={smallImage} className="img-responsive" alt={title} />{" "}
+        <a href={link} title={title} data-lightbox-gallery="gallery1">
+          <div className="hover-text">
+            <h4>{title}</h4>
+          </div>
+          <img
+            src={smallImage}
+            className="img-responsive"
+            alt={title}
+            style={{ width: "1000px", height: "300px" }}
+          />{" "}
+        </a>{" "}
       </div>
     </div>
   );
